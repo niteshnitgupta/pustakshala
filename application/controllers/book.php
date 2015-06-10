79b02b273227155ed6389d22bdb1f4200bab6477
unknown <NIGUPTA@BLRD4926.iis.amadeus.net>
2015-06-10 09:51:38 +0530

    Transaction Book Model

--- application/controllers/book.php
+++ application/controllers/book.php
@@ -3,9 +3,9 @@ class book extends CI_Controller {
 
 	public function TestInsert()
 	{
-		$this->load->model('Transaction_Type_Model');
+		$this->load->model('Transaction_Book_Model');
 
-		$data['query'] = $this->Transaction_Type_Model->get_all_transactions_with_user_address_id();
+		$data['query'] = $this->Transaction_Book_Model->get_all_books_with_transaction_id(1);
 
 		var_dump($data);
 	}
--- application/models/Transaction_Book_Model.php
+++ application/models/Transaction_Book_Model.php
@@ -9,18 +9,25 @@ class Transaction_Book_Model extends CI_Model {
 		parent::__construct();
 	}
 
-	public function get_all_transaction_types()
+	public function add_transaction_book($transaction_id, $book_id)
 	{
-		$query = $this->db->get('transaction_type');
+		$this->transaction_id   = $transaction_id;
+		$this->book_id  		= $book_id;
+		$this->db->insert('transaction_book', $this);
+	}
+
+	public function get_all_books_with_transaction_id($transaction_id)
+	{
+		$this->db->where('transaction_id', $transaction_id);
+		$query = $this->db->get('transaction_book');
 		return $query->result();
 	}
 
-	public function get_transaction_type_id()
+	public function delete_book_from_transaction($transaction_id, $book_id)
 	{
-		$id = $this->input->get_post('id');
-		$this->db->where('id', $id);
-		$query = $this->db->get('transaction_type');
-		return $query->result();
+		$this->db->where('book_id', $transaction_id);
+		$this->db->where('transaction_id', $book_id);
+		$this->db->delete('transaction_book');
 	}
 }
 ?>
